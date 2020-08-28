71f9c9fd66b903fc4c165bc812484e6a77dec499
Joris Mans <joris@astus.be>
2020-08-28 08:57:08 +0200

    remove detection interval label

--- src/xcode/ENA/ENA/Source/Models/Home/HomeRiskCellConfigurator/HomeRiskCellConfigurators/HomeRiskLevelCellConfigurator.swift
+++ src/xcode/ENA/ENA/Source/Models/Home/HomeRiskCellConfigurator/HomeRiskCellConfigurators/HomeRiskLevelCellConfigurator.swift
@@ -18,6 +18,8 @@
 
 import UIKit
 
+// :BE: remove detection interval label
+
 class HomeRiskLevelCellConfigurator: HomeRiskCellConfigurator {
 
 	// MARK: Properties
@@ -106,19 +108,16 @@ class HomeRiskLevelCellConfigurator: HomeRiskCellConfigurator {
 
 		cell.topContainer.isAccessibilityElement = true
 		cell.bodyLabel.isAccessibilityElement = true
-		cell.detectionIntervalLabel.isAccessibilityElement = true
 		cell.updateButton.isAccessibilityElement = true
 
 		cell.topContainer.accessibilityTraits = [.updatesFrequently, .button]
 		cell.bodyLabel.accessibilityTraits = [.updatesFrequently]
-		cell.detectionIntervalLabel.accessibilityTraits = [.updatesFrequently]
 		cell.updateButton.accessibilityTraits = [.updatesFrequently, .button]
 
 		cell.topContainer.accessibilityLabel = cell.titleLabel.text ?? ""
 
 		cell.topContainer.accessibilityIdentifier = AccessibilityIdentifiers.RiskCollectionViewCell.topContainer
 		cell.bodyLabel.accessibilityIdentifier = AccessibilityIdentifiers.RiskCollectionViewCell.bodyLabel
-		cell.detectionIntervalLabel.accessibilityIdentifier = AccessibilityIdentifiers.RiskCollectionViewCell.detectionIntervalLabel
 		cell.updateButton.accessibilityIdentifier = AccessibilityIdentifiers.RiskCollectionViewCell.updateButton
 	}
 
