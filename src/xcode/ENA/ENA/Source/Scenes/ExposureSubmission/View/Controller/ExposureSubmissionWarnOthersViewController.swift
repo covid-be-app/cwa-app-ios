631af84284153148c8e231fafd50b4167e2731f3
Joris Mans <joris@astus.be>
2020-09-09 11:42:23 +0200

    no message

--- src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/ExposureSubmissionWarnOthersViewController.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ExposureSubmission/View/Controller/ExposureSubmissionWarnOthersViewController.swift
@@ -55,6 +55,8 @@ class ExposureSubmissionWarnOthersViewController: DynamicTableViewController, EN
 
 	private func setupView() {
 		navigationItem.title = AppStrings.ExposureSubmissionWarnOthers.title
+		
+		// :BE: accessibility
 		navigationItem.accessibilityLabel = AppStrings.ExposureSubmissionWarnOthers.title
 		navigationFooterItem?.primaryButtonTitle = AppStrings.ExposureSubmissionWarnOthers.continueButton
 		setupTableView()
