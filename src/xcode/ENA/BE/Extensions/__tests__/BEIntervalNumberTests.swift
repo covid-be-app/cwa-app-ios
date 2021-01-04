6126dad4ea7a35c8ee915e72b3ce50289c3b97c7
Joris Mans <joris@astus.be>
2021-01-04 14:04:13 +0100

    Revert "extra test for GMT"
    This reverts commit 82341eff608bc1ea43761d80c3de005c5b6c8159.

--- src/xcode/ENA/BE/Extensions/__tests__/BEIntervalNumberTests.swift
+++ src/xcode/ENA/BE/Extensions/__tests__/BEIntervalNumberTests.swift
@@ -32,18 +32,7 @@ class BEIntervalNumberTests: XCTestCase {
 		
 		XCTAssertEqual(Int(intervalNumber * 600), comparisonValue)
     }
-
-	func testGMT() throws {
-		let dateInt:BEDateInt = 20200805
-		var calendar = Calendar(identifier: .gregorian)
-		calendar.timeZone = TimeZone(secondsFromGMT: 0)!
-		let date = calendar.date(from: DateComponents(year:2020,month:08,day:05))!
-		let intervalNumber = ENIntervalNumber.fromDateInt(dateInt)
-		let comparisonValue = (Int(date.timeIntervalSince1970) / 600) * 600
-		
-		XCTAssertEqual(Int(intervalNumber * 600), comparisonValue)
-	}
-
+	
 	func testToDate() throws {
 		let date = Date()
 		let comparisonValue = TimeInterval((Int(date.timeIntervalSince1970) / 600) * 600)
