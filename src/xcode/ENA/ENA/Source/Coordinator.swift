3f8c3020f060cc793acebe80270b4ad91feff815
Joris Mans <joris@astus.be>
2021-01-07 10:28:08 +0100

    remove onboarding storyboard

--- src/xcode/ENA/ENA/Source/Coordinator.swift
+++ src/xcode/ENA/ENA/Source/Coordinator.swift
@@ -102,14 +102,11 @@ class Coordinator: RequiresAppDependencies {
 		rootViewController.navigationBar.prefersLargeTitles = false
 		rootViewController.setViewControllers(
 			[
-				AppStoryboard.onboarding.initiateInitial { [unowned self] coder in
-					OnboardingInfoViewController(
-						coder: coder,
-						pageType: .togetherAgainstCoronaPage,
-						exposureManager: self.exposureManager,
-						store: self.store
-					)
-				}
+				OnboardingInfoViewController(
+					pageType: .togetherAgainstCoronaPage,
+					exposureManager: self.exposureManager,
+					store: self.store
+				)
 			],
 			animated: false
 		)
